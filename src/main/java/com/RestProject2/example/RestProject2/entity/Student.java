5090996a9fa0283d494512999b5158d6c1a001e1
adeshpokhrel <apokhrel@inventechgroup.com>
2023-07-18 12:10:13 +0545

    created entity

---
+++ src/main/java/com/RestProject2/example/RestProject2/entity/Student.java
@@ -0,0 +1,79 @@
+package com.RestProject2.example.RestProject2.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="student")
+public class Student {
+
+    // define fields
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+    @Column(name="first_mame")
+    private String firstName;
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Student(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    @Column(name="last_name")
+    private String lastName;
+    @Column(name="email")
+    private String email;
+
+
+    // define constructors
+
+    // define getter/setter
+
+    // define toString
+
+
+     public Student(){}
+}
