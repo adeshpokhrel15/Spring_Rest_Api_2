c3c01bd1de4a617f8cb390a779ec9da2d2917c3e
adeshpokhrel <apokhrel@inventechgroup.com>
2023-07-18 10:30:22 +0545

    first commit

---
+++ src/test/java/com/RestProject2/example/RestProject2/RestProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RestProject2.example.RestProject2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestProject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
